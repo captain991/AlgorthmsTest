b48fae62d0049916ac17748b341352c3dc422e30
captain <qiang.z@verifone.cn>
2021-10-13 15:25:37 +0800

    修改了一下默认的toolbar

--- app/src/main/java/com/lotte/algorithmstest/MainActivity.kt
+++ app/src/main/java/com/lotte/algorithmstest/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.lotte.algorithmstest
 
-import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lotte.algorithmstest.databinding.ActivityMainBinding
 
@@ -13,5 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_setting -> {
+                Toast.makeText(this, "Setting clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menu_help -> {
+                Toast.makeText(this, "Help clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
